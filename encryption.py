b4d63effaba5f86aa2231c2bcd4d8a9c93804a15
kimrse <superfosder@gmail.com>
2022-10-31 13:44:42 +1000

    p_r

--- encryption.py
+++ encryption.py
@@ -1,4 +1,14 @@
 # -*- coding: utf-8 -*-
+import timeit
+
+P_ROUND = [
+    0x243F6A88, 0x85A308D3, 0x13198A2E, 0x03707344,
+    0xA4093822, 0x299F31D0, 0x082EFA98, 0xEC4E6C89,
+    0x452821E6, 0x38D01377, 0xBE5466CF, 0x34E90C6C,
+    0xC0AC29B7, 0xC97C50DD, 0x3F84D5B5, 0xB5470917,
+    0x9216D5D9, 0x8979FB1B
+    ]
+
 txt = str(input('text: '))
 
 def get_bits(txt):
@@ -13,11 +23,11 @@ def get_bits(txt):
 def extender(bits):
     bits_divided = []
     bits = ''.join(bits)
-    if len(bits) < 128:
-        while len(bits) < 128:
+    if len(bits) < 256:
+        while len(bits) < 256:
             bits += '0'
-        for i in range(0, 128, 32):   
-            part = bits[i:i+32]
+        for i in range(0, 256, 64):   
+            part = bits[i:i+64]
             bits_divided.append(part)
         return bits_divided
     else:
@@ -54,8 +64,14 @@ bits_ext = extender(txt_bits)
 txt_decoded = decoder(bits_ext)
 
 
+n = 5
+# calculate total execution time
+result = timeit.timeit(stmt='txt_decoded', globals=globals(), number=n)
+
+
 print('source:', txt)
 print('bytes:', list(bytearray(txt, 'utf-8')))
 print('bits:', txt_bits)
 print('extended bits:', bits_ext)
 print('decode:', txt_decoded)
+print(f"Execution time is {result / n} seconds")
